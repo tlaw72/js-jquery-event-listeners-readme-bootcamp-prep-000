2e41797de20885376333e20b578a6444971b267a
T Law <redirect@gmail.com>
2018-02-22 23:03:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt () {
 }
 
 function pressIt () {
-  $('form').on('keydown', function (key) {
+  $('form input').on('keydown', function (key) {
     if (key.which == 47) {
       alert('g was pressed');
       return;
